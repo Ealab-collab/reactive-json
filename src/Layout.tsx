f8f19910866044be21cf93619244079fe445e7fa
Quang-Minh DANG <quang-minh@ea-lab.io>
2025-09-03 17:36:33 +0200

    leosheecool-feature/9--improve-debug: prettier.

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -6,25 +6,23 @@ export const Layout = ({ children }) => {
         <>
             <nav className={styles.navbar}>
                 <a href="/" className={styles.navbarBrand}>
-                    <img
-                        src={reactiveJsonLogo}
-                        className={styles.logo}
-                        alt="Reactive-JSON logo"
-                    />
+                    <img src={reactiveJsonLogo} className={styles.logo} alt="Reactive-JSON logo" />
                     reactive-json demo
                 </a>
                 <ul className={styles.navbarNav}>
                     <li>
-                        <a href="/" className={styles.navLink}>Home</a>
+                        <a href="/" className={styles.navLink}>
+                            Home
+                        </a>
                     </li>
                     <li>
-                        <a href="/demo" className={styles.navLink}>Demo</a>
+                        <a href="/demo" className={styles.navLink}>
+                            Demo
+                        </a>
                     </li>
                 </ul>
             </nav>
-            <div className={styles.container}>
-                {children}
-            </div>
+            <div className={styles.container}>{children}</div>
         </>
     );
 };
